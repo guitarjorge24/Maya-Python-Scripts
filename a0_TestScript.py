0a350f7a2eb9185a8678e1555e768fe06d709eb3
Jorge Luque <guitarjorge24@outlook.com>
2021-02-01 01:12:56 -0500

    Renamed the scripts because Maya couldn't import a script that starts with a number unless you use a workaround. Cleaned up the Test Script file.

--- a0_TestScript.py
+++ a0_TestScript.py
@@ -1,30 +1,38 @@
 from maya import cmds
 
+# create a cube
 cube = cmds.polyCube()
 cubeShape = cube[0]
 
+# create a nurbs circle
 circle = cmds.circle()
 circleShape = circle[0]
-print(cube, circle)
 
+print(cube, circle)
 print type(circle)
 print type(circle[0])
+print(type(None))
 
+# parent the cube to the circle
 cmds.parent(cubeShape, circleShape)
 
-# cmds.setAttr("pCube1.tx", lock = True)
+# lock the cube's translation so it can only be moved by moving the parent
 cmds.setAttr(cubeShape + ".translate", lock=True)
-print(type(None))
+# cmds.setAttr("pCube1.tx", lock = True)
+
+# list test
 myList = ["jorge", 98, True]
-# print("Output: " + myList[0] + myList[1] + myList[2])
-# print("Output: " + str(32))
+# print("Output: " + myList[0] + myList[1] + myList[2] + str(32))
 print type(myList)
 myList.append("one more string")
-print "appended list:", myList
+print "appended list: " + myList
 
+# dictionary test
 applicantDictionary = {"Name": "Jorge Luque", "Position": "Unreal TD", "Phone": 3018217292}
 print applicantDictionary
 print applicantDictionary["Name"]
 
-help(tuple)
+# tuple
 dir(tuple())
+help(tuple)
+
