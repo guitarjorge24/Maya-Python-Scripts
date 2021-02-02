b896d961787f5a3ad8cf2383c8a2819e36b61049
Jorge Luque <guitarjorge24@outlook.com>
2021-02-02 11:46:27 -0500

    Improved renaming script by making sure it doesn't try to rename objects that have already been renamed.

--- a1_OutlinerObjectRenaming.py
+++ a1_OutlinerObjectRenaming.py
@@ -14,11 +14,13 @@ def rename_objects():
 
 	print "________________________"
 	for obj in selection:
-		print ""
+		print("")
+
 		# the parents of the object are added in front and separated by a "|" so we split the string
 		# by "|" and get the last element, which is the current object's name without its parents/full path
 		short_name = obj.split("|")[-1]
 		print "shortName:", short_name
+		print("obj (full name): " + obj)
 
 		# returns the type such as mesh, transform, nurbsCurve, camera, etc
 		# However, when you select a pCube mesh in the outliner, you are actually selecting it's transform node
@@ -29,24 +31,32 @@ def rename_objects():
 		# if there are no children, assign an empty list to "children" instead of assigning "None"
 		children = cmds.listRelatives(obj, children=True, fullPath=True) or []
 		print "Children:", children
+
 		if len(children) == 1:
 			child = children[0]
 			obj_type = cmds.objectType(child)
 		else:
 			obj_type = cmds.objectType(obj)
+
 		print("Actual objType: " + obj_type)
-		# print("actual objType: {}".format(objType))
+
+		# Set the suffix based on object type
 		if obj_type == "mesh":
 			suffix = "geo"  # geometry
 		elif obj_type == "joint":
 			suffix = "jnt"
 		elif obj_type == "camera":
-			print("skipping camera")
-			print "________________________"
+			print("skipping camera\n" + "________________________")
 			continue
 		else:
 			suffix = "grp"
+
+		# if the suffix has already been added to the object, don't add it again
+		if obj.endswith(suffix):
+			print ("skipped " + short_name + " because it already has a suffix\n" + "________________________")
+			continue
+
 		new_name = short_name + "_" + suffix
-		print new_name
+		print ("new name: " + new_name)
 		print "________________________"
 		cmds.rename(obj, new_name)
