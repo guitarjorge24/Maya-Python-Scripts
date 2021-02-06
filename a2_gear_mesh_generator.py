1c24a19cf872ca9b3b1bedf18b927adb231727fa
Jorge Luque <guitarjorge24@outlook.com>
2021-02-06 00:26:53 -0500

    Created the basis of the gear mesh generator script which currently creates a pipe mesh and selects every other face of the outer side. Renamed previous script file names to match python conventions

---
+++ a2_gear_mesh_generator.py
@@ -0,0 +1,22 @@
+from maya import cmds
+
+def generate_gear(teeth=10, length=0.3):
+	subdivisions_axis = teeth * 2
+
+	# Assigns pPipe to transform and polyPipe input node to poly_pipe_constructor
+	transform, poly_pipe_constructor = cmds.polyPipe(subdivisionsAxis = subdivisions_axis)
+
+	# in a default 20-subdivision pipe, the numbering of the faces is as follows:
+	# inner faces: 0-19,  top: 20-39, outer: 40-59, and bottom: 60-79
+	# Thus, the outer faces always start counting at NumberOfSubdivisions * 2 and end at NumberOfSubdivisions * 3 -1
+	# since the upper limit of range() is exclusive, we don't have to do the "-1"
+	outer_side_faces = range(subdivisions_axis*2, subdivisions_axis*3, 2)
+	print outer_side_faces
+
+	cmds.select(clear=True)
+
+	# select all the faces to extrude
+	for face in outer_side_faces:
+		# transform_name.f allows you to access a list containing all the faces of the mesh
+		# "add' means the next face we select is added to the selection w/o deselecting previously selected faces
+		cmds.select("%s.f[%s]" % transform, outer_side_faces, add=True)
