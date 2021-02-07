b808d7f90ae2658224e2239cf12fffe7bff0693d
Jorge Luque <guitarjorge24@outlook.com>
2021-02-06 20:23:37 -0500

    Fixed an issue with accessing the faces, and added the code that extrudes the selected faces.

--- a2_gear_mesh_generator.py
+++ a2_gear_mesh_generator.py
@@ -11,7 +11,7 @@ def generate_gear(teeth=10, length=0.3):
 	# Thus, the outer faces always start counting at NumberOfSubdivisions * 2 and end at NumberOfSubdivisions * 3 -1
 	# since the upper limit of range() is exclusive, we don't have to do the "-1"
 	outer_side_faces = range(subdivisions_axis*2, subdivisions_axis*3, 2)
-	print outer_side_faces
+	print("outer_side_faces: " + str(outer_side_faces))
 
 	cmds.select(clear=True)
 
@@ -19,4 +19,7 @@ def generate_gear(teeth=10, length=0.3):
 	for face in outer_side_faces:
 		# transform_name.f allows you to access a list containing all the faces of the mesh
 		# "add' means the next face we select is added to the selection w/o deselecting previously selected faces
-		cmds.select("%s.f[%s]" % transform, outer_side_faces, add=True)
+		cmds.select("%s.f[%s]" % (transform, face), add=True)
+
+	extrude = cmds.polyExtrudeFacet(localTranslateZ=length)
+	print("extrude: " + extrude)
